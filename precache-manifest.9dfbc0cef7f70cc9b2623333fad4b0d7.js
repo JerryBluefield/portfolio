05d3914e23bc2fbac8b4826911384c49eee8d684
Jerry Blåfield <K9105@student.jamk.fi>
2020-04-02 04:37:25 +0300

    Updates

---
+++ precache-manifest.9dfbc0cef7f70cc9b2623333fad4b0d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8aa97bf81e7e2c22e3e6ee873d49ec",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d1d0fbef1b3cf770963",
+    "url": "/portfolio/static/css/main.a39e5c9e.chunk.css"
+  },
+  {
+    "revision": "e84a76ead667510f7e0b",
+    "url": "/portfolio/static/js/2.a32f6c31.chunk.js"
+  },
+  {
+    "revision": "217a720d620cbec84da392a431eb7fb0",
+    "url": "/portfolio/static/js/2.a32f6c31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d1d0fbef1b3cf770963",
+    "url": "/portfolio/static/js/main.e28355ab.chunk.js"
+  },
+  {
+    "revision": "72153f1981be4ee14a4e",
+    "url": "/portfolio/static/js/runtime-main.55975c6a.js"
+  },
+  {
+    "revision": "4b35a25dd12181c3047ac4763fb8bdd9",
+    "url": "/portfolio/static/media/faceportrait.4b35a25d.png"
+  },
+  {
+    "revision": "2abe51411c79142962080b7140ee033b",
+    "url": "/portfolio/static/media/needles.2abe5141.jpeg"
+  }
+]);
