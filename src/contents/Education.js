63d76932ee5af0d1f457202d45f26e92bb62fb4f
JerryBluefield <35224422+JerryBluefield@users.noreply.github.com>
2024-05-07 16:43:59 +0900

    Update Education.js

--- src/contents/Education.js
+++ src/contents/Education.js
@@ -12,7 +12,8 @@ class Education extends Component {
                     <title>{pagetitle}</title>
                 </Helmet>
             <h1 className="subtopic">My Education</h1>
-            <Widecard title="Bachelor's degree in Information and Communication technologies" where="JAMK University of Applied Sciences" from="August 2017" to="Present"/>
+            <Widecard title="Bachelor's degree in Information and Communication technologies" where="JAMK University of Applied Sciences" from="August 2017" to="June 2021"/>
+            <Widecard title="Language studies at a language school in Kyoto, Japan" where="ARC Academy Kyoto" from="April 2022" to="September 2022"/>
             <Widecard title="Exchange studies in Japan" where="Seinan Gakuin University" from="August 2019" to="December 2019"/>
             <Widecard title="Dual qualification high school graduate | Vocational Degree Electrician" where="Järvenpää High School | KEUDA Vocational School" from="2012" to="2015"/>
             </div>
