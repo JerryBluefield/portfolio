039763cee9b59f4cf65e16c48d3f0c6bc5bb0685
JerryBluefield <35224422+JerryBluefield@users.noreply.github.com>
2024-05-07 15:41:33 +0900

    Update Social.js
    updated social media links

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,7 +4,7 @@ class Social extends Component {
     render() {
         return (
             <div class="social">
-                <a href="https://Linkedin.com/in/jerryblafield" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://www.linkedin.com/in/jerryblaafield/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
                 <a href="https://github.com/JerryBluefield" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
                 <a href="https://Instagram.com/jerrybluefield" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
                 <a href="https://www.facebook.com/jerry.blafield" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
